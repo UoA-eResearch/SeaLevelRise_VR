2349d4d3ffebf8b14fd288846946d7a81f6ee3f7
Bianca Haux <bhau119@UoA.auckland.ac.nz>
2018-05-01 16:06:28 +1200

    Slider.

---
+++ Assets/Slider.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR.InteractionSystem;
+
+
+public class Slider : MonoBehaviour
+{
+
+    public LinearMapping linearMapping;
+    private float currentLinearMapping = float.NaN;
+    private int sliderValue;
+    private GameObject sliderCanvas;
+    private Text sliderText;
+
+    //-------------------------------------------------
+    void Awake()
+    {
+        if (linearMapping == null)
+        {
+            linearMapping = GetComponent<LinearMapping>();
+        }
+
+        sliderCanvas = GameObject.Find("pHCanvas");
+        sliderText = sliderCanvas.GetComponentInChildren<Text>();
+    }
+
+
+    //-------------------------------------------------
+    void Update()
+    {
+        if (currentLinearMapping != linearMapping.value)
+        {
+            currentLinearMapping = linearMapping.value;
+
+            var mappedToPH = (currentLinearMapping - 0.0f) / (1.0f - 0.0f) * (9.0f - 3.0f) + 3.0f;
+            sliderValue = Mathf.RoundToInt(mappedToPH);
+            switch (sliderValue)
+            {
+
+                case 9:
+                    sliderText.text = "pH value: high";
+                    break;
+
+                case 8:
+                case 7:
+                case 6:
+                case 5:
+                case 4:
+                    sliderText.text = "pH value: " + sliderValue;
+                    break;
+
+                case 3:
+                    sliderText.text = "pH value: low";
+                    break;
+
+            }
+        }
+    }
+
+    public int GetPhValue()
+    {
+        return sliderValue;
+    }
+    public string GetPhValueStr()
+    {
+        return sliderText.text;
+    }
+}
--- Assets/rise.cs
+++ Assets/rise.cs
@@ -6,12 +6,13 @@ using UnityEngine.UI;
 
 public class rise : MonoBehaviour {
 
-    string[,] dataArray;
-    GameObject ocean;
-    public string date;
+    private string[,] dataArray;
+    private GameObject ocean;
+    private string date;
     private GameObject dateText;
     private GameObject seaLevelText;
     private int pos = 0;
+    public bool pauseAnimation;
 
     // Use this for initialization
     void Start() {
@@ -31,33 +32,35 @@ public class rise : MonoBehaviour {
         dateText = GameObject.Find("Date");
         seaLevelText = GameObject.Find("SeaLevel");
 
-        InvokeRepeating("animate", 2.0f, 1f);
+        InvokeRepeating("animate", 2.0f, 1);
     }
 
 
     void animate() {
+        if (!pauseAnimation) {
 
-        if (pos >= dataArray.GetLength(0)) {
-            pos = 0;
+            if (pos >= dataArray.GetLength(0))
+            {
+                pos = 0;
+            }
+
+            var date = dataArray[pos, 0];
+            var seaLevel = dataArray[pos, 1];
+
+            ocean.transform.position = new Vector3(0, float.Parse(seaLevel) * 0.01F, 0);
+
+            dateText.GetComponent<Text>().text = "";
+            dateText.GetComponent<Text>().text = date;
+            seaLevelText.GetComponent<Text>().text = "";
+            seaLevelText.GetComponent<Text>().text = seaLevel;
+
+            pos++;
         }
-        
-        var date = dataArray[pos, 0];
-        var seaLevel = dataArray[pos, 1];
-
-        ocean.transform.position = new Vector3(0, float.Parse(seaLevel) * 0.01F, 0);
-
-        dateText.GetComponent<Text>().text = "";
-        dateText.GetComponent<Text>().text = date;
-        seaLevelText.GetComponent<Text>().text = "";
-        seaLevelText.GetComponent<Text>().text = seaLevel;
-
-        pos++;
     }
 
 
 
     // Update is called once per frame
     void Update () {
-        
 	}
 }
